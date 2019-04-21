42d044af4dfb2fb23c5175e894523d66568a7003
小原 涼 <RyoObara@obara-no-MacBook-Pro.local>
2019-04-21 18:50:40 +0900

    urlの切り出しを修正

--- functions/playYoutube.js
+++ functions/playYoutube.js
@@ -9,7 +9,7 @@ const streamOptions = {seek: 0, volume: 0.05};
 module.exports = message => {
   const sender = message.member; // 送信者
   const voiceChannel = sender.voiceChannel; // 送信者の接続しているボイスチャンネル
-  const url = message.content.slice(5); // '!play '以降を切り出す
+  const url = message.content.slice(6); // '!play '以降を切り出す
   if (message.guild.voiceConnection) {
     return message.reply(`再生中だぜ。
 停止したかったら \`!stop\` な。`);
